b5d8f2e0485ed705979950f96fbaec24bd7cc7af
Krishnaraj K <krishvidhyassri@gmail.com>
2022-08-20 20:17:02 +0530

    first commit

---
+++ TestCase1_Title_Compare.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+
+driver = webdriver.Firefox()
+driver.get("https://admin-demo.nopcommerce.com/")
+driver.find_element(By.ID,"Email").clear()
+driver.find_element(By.ID,"Email").send_keys("admin@yourstore.com")
+driver.find_element(By.ID,"Password").clear()
+driver.find_element(By.ID,"Password").send_keys("admin")
+driver.find_element(By.CSS_SELECTOR,".button-1").click()
+
+act_title = driver.title
+exp_title = "Dashboard / nopCommerce administration"
+
+if act_title == exp_title:
+    print("Login Test Passed")
+else:
+    print("Login Test Failed")
+
+driver.close()
