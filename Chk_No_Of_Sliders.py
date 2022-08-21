c539b7d562ee0eff1491121b08e23546428f88c3
Krishnaraj K <krishvidhyassri@gmail.com>
2022-08-21 11:39:43 +0530

    Updated Comment Section

--- Chk_No_Of_Sliders.py
+++ Chk_No_Of_Sliders.py
@@ -8,5 +8,5 @@ driver = webdriver.Firefox(service=serv_obj)
 driver.get("http://automationpractice.com/")
 driver.maximize_window()
 sliders = driver.find_elements(By.CLASS_NAME,'homeslider-container')
-print(len(sliders))
+print(len(sliders)) #Total number of sliders in the webpage
 driver.close()
