c2a96ac6c56ec5ba35ca49629e79d9fa25dcc85f
Krishnaraj K <krishvidhyassri@gmail.com>
2022-09-03 12:29:59 +0530

    Updated Driver

--- test_Chk_No_of_links.py
+++ test_Chk_No_of_links.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
-# serv_obj = Service("C:\SeleniumDrivers\geckodriver-v0.31.0-win64\geckodriver")
-# driver = webdriver.Firefox(service=serv_obj)
-serv_obj = Service("C:\SeleniumDrivers\chromedriver_win32\chromedriver")
-driver = webdriver.Chrome(service=serv_obj)
+serv_obj = Service("C:\SeleniumDrivers\geckodriver-v0.31.0-win64\geckodriver")
+driver = webdriver.Firefox(service=serv_obj)
+# serv_obj = Service("C:\SeleniumDrivers\chromedriver_win32\chromedriver")
+# driver = webdriver.Chrome(service=serv_obj)
 
 driver.get("http://automationpractice.com/")
 driver.maximize_window()
